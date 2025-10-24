700aa993a58a90722682473459a5fa7374206bd7
safiye-dalkiran <safiyedal360@gmail.com>
2025-10-24 03:29:10 +0300

    center profile image on mobile

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -8,7 +8,6 @@ export default function Hero() {
   return (
     <div className="flex flex-col gap-6 pt-10 sm:pt-15 container mx-auto px-4">
 
-      {/* Başlık */}
       <div className="flex flex-col sm:flex-row items-center gap-2 pt-5 sm:pt-10 text-center md:text-left">
         <span className="block w-20 sm:w-28 h-0.5 dark:bg-[#B7AAFF] bg-[#3730A3]"></span>
         <h1 className="font-medium text-2xl sm:text-3xl dark:text-[#B7AAFF] text-[#3730A3]">
@@ -16,10 +15,8 @@ export default function Hero() {
         </h1>
       </div>
 
-      {/* İçerik */}
+    
       <div className="flex flex-col md:flex-row w-full gap-6 md:gap-2 items-center md:items-start">
-
-        {/* Sol bölüm */}
         <div className="flex flex-col gap-6 md:gap-8 w-full md:w-3/5 flex-1 text-center md:text-left">
           <h1 className="text-4xl sm:text-5xl md:text-8xl font-bold dark:text-[#AEBCCF] text-[#1F2937]">
             {content.heroSection.text1}
@@ -30,14 +27,15 @@ export default function Hero() {
           <SosialButtons />
         </div>
 
-        {/* Sağ bölüm (mobilde gizli) */}
-        <div className="w-full md:w-2/5 hidden md:flex justify-center">
-          <img
-            src={content.heroSection.profileImage}
-            alt="Profile"
-            className="rounded-xl w-60 sm:w-72 md:w-75 h-auto"
-          />
-        </div>
+        
+        <div className="w-full md:w-2/5 flex justify-center md:justify-start">
+  <img
+    src={content.heroSection.profileImage}
+    alt="Profile"
+    className="rounded-full w-60 sm:w-72 md:w-75 h-auto"
+  />
+</div>
+
       </div>
     </div>
   );
